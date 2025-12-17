7c9a4fed9201d31066487c1a5aba1235d738cb0d
Pannnomaly <supawith007@gmail.com>
2025-12-17 15:23:11 +0700

    add vercel.json, icon, @theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center gap-y-3">
-        <h1 className="text-2xl font-bold">Hello World!</h1>
+        <h1 className="text-2xl font-bold hover:bg-(--color-main1) transition duration-300 ease-in-out">Hello World!</h1>
         <Button variant="ghost">Click me</Button>
       </div>
     </>
