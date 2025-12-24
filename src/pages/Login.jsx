376d2d08416d4b90aae32fab864a31ecbb8341d4
NoobDevMBA <kanommudmi@gmail.com>
2025-12-24 21:41:26 +0700

    feat: revise code and button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Login = () => {
@@ -6,24 +5,26 @@ const Login = () => {
     <>
       <main className="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-3xl font-bold text-center mb-6">SIGN IN</h1>
-        <div className="container w-full max-w-2xl bg-white shadow-md rounded-xl p-8 border border-gray-200">
-          <div className="flex justify-end">
+        <div className="container w-full max-w-2xl bg-white shadow-md p-8 border border-gray-200">
+          <div className="flex justify-end gap-2 my-2">
+            <p className="text-gray-500 text-sm ">You're new here? </p>
             <a href="./registration.html">
-              <Button className="text-xl text-end font-semibold mb-1">Create an account</Button>
+              <p className="text-sm text-end font-semibold underline underline-offset-2 mb-1">
+                Create an account
+              </p>
             </a>
           </div>
-          <p className="text-gray-500 text-sm mb-6">Enter your email below to create your account</p>
 
           <div className="space-y-4">
             <input
               type="email"
               placeholder="Email"
-              className="w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-black focus:outline-none"
+              className="w-full px-4 py-3 border focus:ring-2 focus:ring-black focus:outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className="w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-black focus:outline-none"
+              className="w-full px-4 py-3 border  focus:ring-2 focus:ring-black focus:outline-none"
             />
           </div>
 
@@ -41,21 +42,21 @@ const Login = () => {
 
           <div className="grid grid-cols-2 gap-4 mb-6">
             <a href="./room-detail.html">
-              <Button className="w-full border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-50">
+              <button className="w-full border  py-2 flex items-center justify-center gap-2 hover:bg-gray-50">
                 <img src="https://www.svgrepo.com/show/475656/google-color.svg" className="w-5" />
-              </Button>
+              </button>
             </a>
             <a href="./room-detail.html">
-              <Button className="w-full border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-50">
+              <button className="w-full border  py-2 flex items-center justify-center gap-2 hover:bg-gray-50">
                 <img src="https://files.svgcdn.io/streamline-color/meta.svg" className="w-5" />
-              </Button>
+              </button>
             </a>
           </div>
 
           <a href="./room-detail.html">
-            <Button className="w-full bg-[#DACDB9] text-gray-700 font-semibold py-3 rounded-lg hover:bg-[#e9decf] transition">
+            <button className="w-full bg-[var(--color-main4)] text-gray-700 font-medium py-3  hover:bg-[#e9decf] transition">
               Log in
-            </Button>
+            </button>
           </a>
         </div>
       </main>
