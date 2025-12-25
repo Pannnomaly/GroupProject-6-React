828bbe8db57a419ead0e8dca60ea948ca7f797dc
evefers <evefers@gmail.com>
2025-12-25 09:42:23 +0700

    Updated booking cfm page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
-import RoomDetail from "./pages/RoomDetail";
+
 
 export default function App ()
 {
   return (
     <div className="min-h-screen">
-      {/* <Outlet /> */}
-      <RoomDetail />
+ <Outlet />
     </div>
   );
 }
