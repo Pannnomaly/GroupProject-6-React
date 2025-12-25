4b984133c73a7e72617fc86c1211a27b0c4c89e3
evefers <evefers@gmail.com>
2025-12-25 13:05:23 +0700

    update app.jsx to include my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
-
+import RoomDetail from "./pages/RoomDetail";
+import BookingDetail from "./pages/BookingDetail.jsx";
+import BookingConfirm from "./pages/BookingConfirm.jsx";
 
 export default function App ()
 {
   return (
     <div className="min-h-screen">
- <Outlet />
+      {/* <Outlet /> */}
+      <RoomDetail />
+      <BookingDetail />
+      <BookingConfirm />
     </div>
   );
 }
