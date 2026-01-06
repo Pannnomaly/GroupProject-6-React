62633c9b5a1cda3bc40a0dbe1138292641cc79a0
mag1939 <songsak.thawaro@gmail.com>
2026-01-06 22:00:39 +0700

    create Sidebar and AdminDashboard

---
+++ src/components/SidebarApp.jsx
@@ -0,0 +1,64 @@
+import { LayoutDashboard, Bed, NotebookPen, User } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "./ui/sidebar"
+import { Link } from "react-router-dom"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Room Lists",
+    url: "#",
+    icon: Bed,
+  },
+  {
+    title: "Booking",
+    url: "#",
+    icon: NotebookPen,
+  },
+]
+
+export default function SideBar() {
+
+    return (
+        <Sidebar>
+            <SidebarHeader>
+                <img className="size-fit" src="hotel-logo.png" alt="" />
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Main Menu</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <Link to={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <Link to="#">
+                                <User />
+                                <span>Joe Dorn</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
