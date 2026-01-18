0c4c63ec63af4886ff02957cc523add7e8bdfe2a
Pannnomaly <supawith007@gmail.com>
2026-01-18 20:14:10 +0700

    fix(user and user-card): fix styling ui

--- src/components/User-card.jsx
+++ src/components/User-card.jsx
@@ -1,6 +1,6 @@
 export default function UserCard({ imagePath, imageAlt, roomName, checkIn, duration, guests, price }) {
   return (
-    <div className="flex items-center gap-x-10 p-3 hover:bg-main6 hover:text-white border-b-2 mb-2 transition duration-300 ease-in-out">
+    <div className="flex items-center gap-x-10 p-3 hover:bg-(--color-main3) border-b-2 mb-2 text-shadow-2xs transition duration-300 ease-in-out">
       <div>
         <img
           src={imagePath}
@@ -12,22 +12,22 @@ export default function UserCard({ imagePath, imageAlt, roomName, checkIn, durat
       </div>
       <div className="flex flex-col gap-y-0.5 mt-8 ">
         <div>
-          <p className="text-2xl  text-main7 font-semibold ">{roomName ? roomName : "Room data"}</p>
+          <p className="text-2xl  text-black font-semibold ">{roomName ? roomName : "Room data"}</p>
         </div>
         <div>
-          <span className="font-semibold text-lg text-main2 group-hover:text-white transition-colors">Check in: </span>
-          <span className="text-main6 group-hover:text-white transition-colors">{checkIn ? checkIn : "Today"}</span>
+          <span className="font-semibold text-lg text-black">Check in: </span>
+          <span className="text-black">{checkIn ? checkIn : "Today"}</span>
         </div>
         <div>
-          <span className="font-semibold text-lg text-[#484848] group-hover:text-white transition-colors">Duration: </span>
-          <span className="text-main6 group-hover:text-white transition-colors">{duration ? duration : "Short"}</span>
+          <span className="font-semibold text-lg text-black">Duration: </span>
+          <span className="text-black">{duration ? duration : "Short"}</span>
         </div>
         <div>
-          <span className="font-semibold text-lg text-[#484848] group-hover:text-white transition-colors">Guests: </span>
-          <span className="text-main6 group-hover:text-white transition-colors">{guests ? guests : "0"}</span>
+          <span className="font-semibold text-lg text-black ">Guests: </span>
+          <span className="text-black">{guests ? guests : "0"}</span>
         </div>
         <div>
-          <p className="font-semibold text-lg text-[#484848] group-hover:text-white transition-colors">$ {price ? price : "0"} USD</p>
+          <p className="font-semibold text-lg text-black ">$ {price ? price : "0"} USD</p>
         </div>
       </div>
     </div>
